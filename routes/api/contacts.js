db77f7d83bf9fa07e576198ffaec4209058a5c26
Kate Panchenko <k.panchenko12345@gmail.com>
2023-05-24 16:02:40 +0300

    get та get by id запити готові

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,48 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const contactsService = require("../../models/contacts");
+const { HttpError } = require("../../helpers");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (req, res, next) => {
+	try {
+		const result = await contactsService.listContacts();
+		res.json(result);
+	} catch (error) {
+		res.status(500).json({
+			message: "Server error",
+		});
+	}
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+	try {
+		const { contactId } = req.params;
+		const result = await contactsService.getContactById(contactId);
+		if (!result) {
+			throw HttpError(404, "Not Found");
+		}
+		res.json(result);
+	} catch (error) {
+		const { status = 500, message = "Server error" } = error;
+		res.status(status).json({
+			message,
+		});
+	}
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", async (req, res, next) => {
+	const result = await cotacts.addContact(req.body);
+	res.status(201).json(result);
+});
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+	res.json({ message: "template message" });
+});
 
-module.exports = router
+router.put("/:contactId", async (req, res, next) => {
+	res.json({ message: "template message" });
+});
+
+module.exports = router;
