7f6a80a8de13c19f6259d7c7f46597e85a3e0c75
Kate Panchenko <110391066+Kate-Panchenko@users.noreply.github.com>
2023-06-08 12:52:03 +0300

    підключений сервер

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+//zQt7t6wdIVS0sVew
+
+const DB_HOST =
+	"mongodb+srv://03-mongodb:zQt7t6wdIVS0sVew@cluster0.1uraokm.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+// mongoose.connect(process.env.DB_HOST, {
+// 	useNewUrlParser: true,
+// 	useCreateIndex: true,
+// 	useUnifiedTopology: true,
+// });
+
+mongoose
+	.connect(DB_HOST)
+	.then(() => {
+		app.listen(3000), console.log("Database connection successful");
+	})
+	.catch((err) => {
+		console.log(err.message);
+		process.exit(1);
+	});
